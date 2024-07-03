33366986528886a66c69fb7f92d985c708466f20
Thanh Hùng Lý <thanhhung270502@gmail.com>
2024-07-03 14:52:02 +0700

    Remove something

--- src/passport.js
+++ src/passport.js
@@ -1,14 +1,11 @@
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const passport = require('passport');
 
-const GOOGLE_CLIENT_ID = '166927528877-slh38kg3sq5v1d6gtcbgm9o0284p7kp4.apps.googleusercontent.com';
-const GOOGLE_CLIENT_SECRET = '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';
-
 passport.use(
     new GoogleStrategy(
         {
-            clientID: GOOGLE_CLIENT_ID,
-            clientSecret: GOOGLE_CLIENT_SECRET,
+            clientID: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
             callbackURL: '/auth/google/callback',
             scope: ['profile', 'email'],
         },
